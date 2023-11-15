daf22f4bbba2fbb2eef49852850053b092df2635
azhu000 <azhu000@citymail.cuny.edu>
2023-11-14 22:01:57 -0500

    added DB connection and querying from database

---
+++ dbModels.py
@@ -0,0 +1,24 @@
+from click import password_option
+from flask import Flask, g, redirect, render_template, url_for, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LOGIN_MESSAGE, UserMixin, login_user, LoginManager, login_required, logout_user,current_user
+from sqlalchemy import ForeignKey
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
+
+# Initialize the database
+db = SQLAlchemy()
+
+class user(db.Model, UserMixin):
+    userID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    zipcode = db.Column(db.Integer, nullable=False)
+    usertype = db.Column(db.Integer, nullable=False, default=0)
+
+
