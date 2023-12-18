63db17195bcbc0d3724a49c92da838cf56a797f4
Konrad Zielinski <konradzielinski214@gmail.com>
2023-12-18 13:53:28 -0500

    adjusted database init sql query

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,17 +1,16 @@
-
 -- DROP table if exists "user";
-Create table if not exists "user" (
-	"userID" SERIAL,
-	"name" VARCHAR(100) NOT NULL,
-	"email" VARCHAR(100) NOT NULL,
-	"password" VARCHAR(100) NOT NULL,
-	"address" VARCHAR(100) NOT NULL,
-	"city" VARCHAR(50) NOT NULL,
-	"state" VARCHAR(2) NOT NULL,
-	"zipcode" INT NOT NULL,
-	"usertype" INT NOT NULL DEFAULT 0,
-	PRIMARY KEY ("userID")
-);
+-- Create table if not exists "user" (
+-- 	"userID" SERIAL,
+-- 	"name" VARCHAR(100) NOT NULL,
+-- 	"email" VARCHAR(100) NOT NULL,
+-- 	"password" VARCHAR(100) NOT NULL,
+-- 	"address" VARCHAR(100) NOT NULL,
+-- 	"city" VARCHAR(50) NOT NULL,
+-- 	"state" VARCHAR(2) NOT NULL,
+-- 	"zipcode" INT NOT NULL,
+-- 	"usertype" INT NOT NULL DEFAULT 0,
+-- 	PRIMARY KEY ("userID")
+-- );
 
 -- INSERT INTO "user" ("name", "email", "password", "address", "city", "state", "zipcode", "usertype")
 -- VALUES ('hi', 'something@example.com', '123', 'WEIRD ST', 'NYC', 'NY', 10001, 0);
@@ -19,13 +18,13 @@ Create table if not exists "user" (
 -- SELECT * FROM "user"
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- DROP table if exists "order";
+DROP table if exists "order";
 create table if not exists "order"(
 	"orderID" serial,
 	"userID" int,
 	"orderDate" date NOT NULL default current_date,
 	"status" varchar(15),
-	"totalAmount" numeric(6,2),
+	"totalAmount" numeric(5,0),
 	primary key ("orderID"),
 	foreign key ("userID") references "user" ("userID") on Update cascade
 );
@@ -35,14 +34,15 @@ create table if not exists "order"(
 -- SELECT * FROM "order"
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- DROP table if exists "product";
+DROP table if exists "product";
 CREATE table if not exists "product" (
 	"productID" serial,
 	"sellerID" INT NOT NULL,
 	"itemName" varchar(100) NOT NULL,
 	"itemDesc" varchar(300),
-	"price" numeric(6,2) NOT NULL,
+	"price" numeric(4,0) NOT NULL,
 	"stock" int,
+	"category" int,
 	Primary Key ("productID"),
 	Foreign Key ("sellerID") References "user" ("userID") ON update CASCADE);
 	
@@ -52,7 +52,7 @@ CREATE table if not exists "product" (
 -- SELECT * FROM "product"
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- DROP table if exists "review";
+DROP table if exists "review";
 create table if not exists "review" (
 	"reviewID" serial,
 	"userID" INT not NULL,
@@ -70,12 +70,12 @@ create table if not exists "review" (
 -- SELECT * FROM "review"
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- DROP table if exists "payment";
+DROP table if exists "payment";
 create table if not exists "payment" (
 	"transactionID" serial,
 	"orderID" int,
 	"paymentMethod" varchar(100) not null,
-	"amount" numeric (6,2) not null,
+	"amount" numeric (5,0) not null,
 	"status" varchar (100) not null,
 	primary key ("transactionID"),
 	foreign key ("orderID") references "order" ("orderID") on update cascade
@@ -86,7 +86,7 @@ create table if not exists "payment" (
 -- SELECT * FROM "payment"
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- DROP table if exists "orderItem";
+DROP table if exists "orderItem";
 create table if not exists "orderItem"(
 	"orderItemID" serial,
 	"orderID" int,
@@ -102,7 +102,7 @@ create table if not exists "orderItem"(
 -- SELECT * FROM "orderItem"
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- DROP table if exists "cart";
+DROP table if exists "cart";
 create table if not exists "cart" (
 	"cartID" serial,
 	"userID" int,
@@ -115,7 +115,7 @@ create table if not exists "cart" (
 -- SELECT * FROM "cart"
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
--- DROP table if exists "cartItems";
+DROP table if exists "cartItems";
 create table if not exists "cartItems" (
 	"cartItemID" serial,
 	"cartID" int,
@@ -133,18 +133,15 @@ create table if not exists "cartItems" (
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
 -- DROP table if exists "category";
-CREATE table if not exists "category"(
-	"categoryID" serial,
-	"parentCategoryID" INT NOT NULL,
-	"name" varchar(100) NOT NULL,
-	"desc" varchar(300),
-	Primary Key ("categoryID"),
-	Foreign Key ("parentCategoryID") References "category" ("categoryID")
-);
+-- CREATE table if not exists "category"(
+-- 	"categoryID" serial,
+-- 	"name" varchar(100) NOT NULL,
+-- 	"desc" varchar(300),
+-- 	Primary Key ("categoryID")
+-- );
 
 -- INSERT INTO "category" ("parentCategoryID", "name", "desc") 
 -- values (1, 'chairs', 'something to sit on'),
 -- 	   (1, 'stools', 'a chair with no backrest'),
 -- 	   (2, 'table', 'somewhere to place goods');
 -- SELECT * FROM "category"
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
